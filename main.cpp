685b4d4056f4013f238ec0b8c19b1fa5a31f6745
Hrishikesh-Mhaiskar <hrishikeshm004@gmail.com>
2024-04-18 19:15:25 +0530

    Basic UI structure

--- main.cpp
+++ main.cpp
@@ -4,9 +4,50 @@ using namespace std;
 
 int main()
 {
-    string name;
-    getline(cin, name);
+    cout << "\n\n-----------HOSPITAL MANAGEMENT SYSTEM----------------" << endl;
+    cout << endl;
+    cout << "Welcome to Hospital Management System CLI!" << endl;
+    cout << endl;
 
+    while(true)
+    {
+        int input;
 
-    search_doctor_by_name(name);
+        cout << "--------------------------------------------\n" << endl;
+        cout << "Please select amongst the following options:" << endl;
+        cout << "1. Doctor Manager" << endl;
+        cout << "2. Staff Manager" << endl;
+        cout << "3. Patient Manager" << endl;
+        cout << "4. exit" << endl;
+        cout << "--------------------------------------------\n" << endl;
+
+        cout << "Enter option: ";
+        cin >> input;
+
+        if(input == 1)
+        {
+            // doctor manager
+        }
+        else if(input == 2)
+        {
+            // staff manager
+        }
+        else if(input == 3)
+        {
+            // patient manager
+        }
+        else if(input == 4)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Invalid Input! Pls enter a valid input!" << endl;
+            cout << endl;
+        }
+    }
+
+    cout << "\n\n--------------THANK YOU--------------" << endl;
+
+    return 0;
 }
