d8d4d4ec8a1c10459b347d191a7628c0dbed3ae6
Hrishikesh-Mhaiskar <hrishikeshm004@gmail.com>
2024-04-18 18:12:19 +0530

    sorting code into classes and main function

--- classes.h
+++ classes.h
@@ -1,3 +1,6 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
 #include <iostream>
 
 using namespace std;
@@ -8,7 +11,7 @@ class Person
         string name;
         int age;
         char gender;
-        int contact;
+        long long int contact;
 
     public:
         virtual void display_details()
@@ -71,7 +74,7 @@ class Doctor : public Employee
             Employee::count++;
         }
 
-        Doctor(string name, int age, char gender, int contact, double salary, double years_experience, string specialization)
+        Doctor(string name, int age, char gender, long long int contact, double salary, double years_experience, string specialization)
         {
             Employee::count++;
 
@@ -116,7 +119,7 @@ class Staff : public Employee
             Employee::count++;
         }
 
-        Staff(string name, int age, char gender, int contact, double salary, double years_experience, string job_description)
+        Staff(string name, int age, char gender, long long int contact, double salary, double years_experience, string job_description)
         {
             Employee::count++;
 
@@ -196,7 +199,7 @@ class Patient : public Person
             Patient::count++;
         }
 
-        void set_data(string name, int age, char gender, int contact, Doctor doctor_assigned)
+        Patient(string name, int age, char gender, long long int contact, Doctor doctor_assigned)
         {
             Patient::count++;
 
@@ -207,7 +210,17 @@ class Patient : public Person
             this->patient_id = count;
             this->doctor_assigned = doctor_assigned;
             this->is_appointment_scheduled = false;
+        }
 
+        void set_data(string name, int age, char gender, long long int contact, Doctor doctor_assigned)
+        {
+            this->name = name;
+            this->age = age;
+            this->gender = gender;
+            this->contact = contact;
+            this->patient_id = count;
+            this->doctor_assigned = doctor_assigned;
+            this->is_appointment_scheduled = false;
         }
 
         ~Patient()
@@ -216,5 +229,37 @@ class Patient : public Person
         }
 };
 
+
+
 int Employee::count = 0;
 int Patient::count = 0;
+
+Doctor doctor1("Dr. Rajesh Kumar", 40, 'M', 9876543210, 60000.0, 15.0, "Neurology");
+Doctor doctor2("Dr. Pooja Patel", 45, 'F', 9876543211, 55000.0, 12.0, "Oncology");
+Doctor doctor3("Dr. Vikram Singh", 50, 'M', 9876543212, 70000.0, 20.0, "Orthopedics");
+Doctor doctor4("Dr. Neha Sharma", 55, 'F', 9876543213, 50000.0, 10.0, "Pediatrics");
+Doctor doctor5("Dr. Ritu Reddy", 60, 'F', 9876543214, 65000.0, 18.0, "Dermatology");
+
+Staff staff1("Amit Patel", 25, 'M', 9876543200, 35000.0, 4.5, "Nurse");
+Staff staff2("Priya Sharma", 30, 'F', 9876543201, 30000.0, 3.0, "Receptionist");
+Staff staff3("Rahul Singh", 35, 'M', 9876543202, 40000.0, 5.5, "Janitor");
+Staff staff4("Sneha Gupta", 28, 'F', 9876543203, 32000.0, 2.0, "Lab Technician");
+Staff staff5("Ananya Reddy", 40, 'F', 9876543204, 38000.0, 6.0, "Pharmacist");
+
+Patient patient1("Rahul", 25, 'M', 9876543200, doctor1);
+Patient patient2("Priya", 30, 'F', 9876543201, doctor2);
+Patient patient3("Vikram", 35, 'M', 9876543202, doctor3);
+Patient patient4("Neha", 28, 'F', 9876543203, doctor4);
+Patient patient5("Ritu", 40, 'F', 9876543204, doctor5);
+Patient patient6("Amit", 32, 'M', 9876543205, doctor1);
+Patient patient7("Kavita", 28, 'F', 9876543206, doctor2);
+Patient patient8("Suresh", 33, 'M', 9876543207, doctor3);
+Patient patient9("Mona", 38, 'F', 9876543208, doctor4);
+Patient patient10("Rajesh", 45, 'M', 9876543209, doctor5);
+
+
+Doctor doctors[] = {doctor1, doctor2, doctor3, doctor4, doctor5};
+Staff staff[] = {staff1, staff2, staff3, staff4, staff5};
+Patient patients[] = {patient1, patient2, patient3, patient4, patient5, patient6, patient7, patient8, patient9, patient10};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "classes.h"
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        doctors[i].display_details();
+    }
+}
